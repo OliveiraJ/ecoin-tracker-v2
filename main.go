05bf18e32405aca0cf36b061ed87b3ef7488789c
OliveiraJ <jordansilva102@gmail.com>
2021-07-27 21:06:59 -0300

    primeira build e anterior a http

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/OliveiraJ/ecoin-tracker-v2/src"
 )
 
 const URL string = "https://bscscan.com/token/0x4cbdfad03b968bf43449d0908f319ae4a5a33371?a=0x000000000000000000000000000000000000dead"
 
 func main() {
-	src.GetData(URL)
+	scrapeTime := time.Duration(1) * time.Minute
+	ticker := time.NewTicker(scrapeTime)
+
+	for range ticker.C {
+		log.Println("Começando nova leitura...")
+		src.GetData(URL)
+		log.Println("Leitura efetuda")
+
+	}
 }
